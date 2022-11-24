c0f6962fdfde339c50db670815f83632b4077cd5
astrhsptl <dctrback@bk.ru>
2022-11-24 13:05:38 +0300

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
-import Temp from "./components/temp";
-import {BrowserRouter,Route, Routes} from "react-router-dom";
-import LoginComponent from "./components/LoginComponent";
-import RegiterComponent from "./components/RegiterComponent";
+import {BrowserRouter,Outlet,Route, Routes,} from "react-router-dom";
+import { paths } from "./configure/pages";
+
 
 function App() {
   let [user, setUser] = useState({
@@ -12,17 +11,21 @@ function App() {
     access: '',
     refresh: '',
   });
+
   useEffect(() => {
-    console.log(`user: ${user.access}`)
+    console.log(`user: ${user.access}`) 
   }, [user])
   return (
+    <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Temp user={user}/>}/>  {/*Start Page*/}
-          <Route path="/login" element={<LoginComponent user={user} setUser={setUser}/>}/> {/* Login Form */}
-          <Route path="/register" element={<RegiterComponent user={user} setUser={setUser}/>}/> {/* Register form */}
+          {paths.map(route => 
+            <Route path={route.path} element={<route.component user={user} setUser={setUser}/>}/>
+          )}
         </Routes>
       </BrowserRouter>
+      <Outlet/>
+    </div>
   );
 }
 
